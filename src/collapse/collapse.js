1cd667b998223632231922cf0191c2b4d6432c3a
Christoffer Mansfield <christoffer.mansfield@gmail.com>
2015-08-06 12:02:24 +0200

    fix(collapse): Modified collapse to use jQuery slide

--- src/collapse/collapse.js
+++ src/collapse/collapse.js
@@ -1,75 +1,51 @@
 angular.module('ui.bootstrap.collapse', ['ui.bootstrap.transition'])
 
-  .directive('collapse', ['$transition', function ($transition) {
+  .directive('collapse', ['$q', function ($q) {
 
-    return {
-      link: function (scope, element, attrs) {
+      return {
+          link: function (scope, element, attrs) {
 
-        var initialAnimSkip = true;
-        var currentTransition;
+              var initialAnimSkip = true;
 
-        function doTransition(change) {
-          var newTransition = $transition(element, change);
-          if (currentTransition) {
-            currentTransition.cancel();
+              function expand() {
+                  if (initialAnimSkip) {
+                      initialAnimSkip = false;
+                      expandDone();
+                  } else {
+                      element.slideDown(function () {
+                          expandDone();
+                      });
+                  }
+              }
+
+              function expandDone() {
+                  element.css({ height: 'auto' });
+              }
+
+              function collapse() {
+                  if (initialAnimSkip) {
+                      initialAnimSkip = false;
+                      collapseDone();
+                  } else {
+                      var deferred = $q.defer();
+                      element.slideUp({ done: deferred.resolve });
+                      deferred.promise.then(function () {
+                          collapseDone();
+                      });
+                  }
+              }
+
+              function collapseDone() {
+                  element.height(0);
+              }
+
+              scope.$watch(attrs.collapse, function (shouldCollapse) {
+                  if (shouldCollapse) {
+                      collapse();
+                  } else {
+                      expand();
+                  }
+              });
           }
-          currentTransition = newTransition;
-          newTransition.then(newTransitionDone, newTransitionDone);
-          return newTransition;
-
-          function newTransitionDone() {
-            // Make sure it's this transition, otherwise, leave it alone.
-            if (currentTransition === newTransition) {
-              currentTransition = undefined;
-            }
-          }
-        }
-
-        function expand() {
-          if (initialAnimSkip) {
-            initialAnimSkip = false;
-            expandDone();
-          } else {
-            element.removeClass('collapse').addClass('collapsing');
-            doTransition({ height: element[0].scrollHeight + 'px' }).then(expandDone);
-          }
-        }
-
-        function expandDone() {
-          element.removeClass('collapsing');
-          element.addClass('collapse in');
-          element.css({height: 'auto'});
-        }
-
-        function collapse() {
-          if (initialAnimSkip) {
-            initialAnimSkip = false;
-            collapseDone();
-            element.css({height: 0});
-          } else {
-            // CSS transitions don't work with height: auto, so we have to manually change the height to a specific value
-            element.css({ height: element[0].scrollHeight + 'px' });
-            //trigger reflow so a browser realizes that height was updated from auto to a specific value
-            var x = element[0].offsetWidth;
-
-            element.removeClass('collapse in').addClass('collapsing');
-
-            doTransition({ height: 0 }).then(collapseDone);
-          }
-        }
-
-        function collapseDone() {
-          element.removeClass('collapsing');
-          element.addClass('collapse');
-        }
-
-        scope.$watch(attrs.collapse, function (shouldCollapse) {
-          if (shouldCollapse) {
-            collapse();
-          } else {
-            expand();
-          }
-        });
-      }
-    };
+      };
   }]);
